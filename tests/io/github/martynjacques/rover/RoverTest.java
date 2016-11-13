84882349acb57f46cc147969260ffaa51082cf7c
Martyn Jacques <martynjacques@gmail.com>
2016-11-13 14:22:18 +0000

    Added provided test cases

--- tests/io/github/martynjacques/rover/RoverTest.java
+++ tests/io/github/martynjacques/rover/RoverTest.java
@@ -4,23 +4,24 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class RoverTest {
-
+    
     @Test
-    public void canProvideCurrentLocationAsString() {
+    public void testFirstProvidedTestCase() {
     	Coordinates plateauBounds = new Coordinates(5, 5);
         Plateau plateau = new Plateau(plateauBounds);
-        Coordinates startingPosition = new Coordinates(3,3);
-
+        Coordinates startingPosition = new Coordinates(1, 2);
         Rover marsRover = new Rover(startingPosition, Direction.NORTH, plateau);
 
-        Assert.assertEquals("3 3 N", marsRover.toString());
+        marsRover.executeInstructions("LMLMLMLMM");
+
+        Assert.assertEquals("1 3 N", marsRover.toString());
     }
     
     @Test
-    public void canRunCommandWithMultipleInstructions() {
+    public void testSecondProvidedTestCase() {
     	Coordinates plateauBounds = new Coordinates(5, 5);
         Plateau plateau = new Plateau(plateauBounds);
-        Coordinates startingPosition = new Coordinates(3,3);
+        Coordinates startingPosition = new Coordinates(3, 3);
         Rover marsRover = new Rover(startingPosition, Direction.EAST, plateau);
 
         marsRover.executeInstructions("MMRMMRMRRM");
